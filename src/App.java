75e982354f3fe849d73fb11085b07e50a0b17a6e
Zoran-Petrov <zoran_782002@yahoo.com>
2019-04-12 21:22:29 +0300

    added Stage object

--- src/App.java
+++ src/App.java
@@ -2,11 +2,15 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    Stage stage;
     public static void main(String[] args) {
     }
 
     @Override
     public void start(Stage primaryStage) {
+        stage = primaryStage;
 
     }
+
+
 }
